36b0b193657b38ea70c452beceae1500a0a6f414
gaoyuan <gaoyuan@megvii.com>
2019-06-19 17:35:27 +0800

    link_list and sort

---
+++ link_list.py
@@ -0,0 +1,154 @@
+import pdb
+
+
+class Node(object):
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def __repr__(self):
+        return str(self.value)
+
+
+def print_link_list_value(head):
+    while head:
+        print(head, end=' ')
+        head = head.next
+    print()
+
+
+def get_last_k(head, k):
+    p_slow = p_fast = head
+    for _ in range(k):
+        p_fast = p_fast.next
+
+    while p_fast:
+        p_fast = p_fast.next
+        p_slow = p_slow.next
+
+    return p_slow
+
+
+def gene_link_list(int_list):
+    if len(int_list) == 0:
+        return None
+    nodes = list()
+    for value in int_list:
+        nodes.append(Node(value))
+    for i in range(len(nodes) - 1):
+        nodes[i].next = nodes[i + 1]
+    head = nodes[0]
+    return head
+
+
+def merge_two_sorted_list(head1, head2):
+    if head1 is None:
+        return head2
+    if head2 is None:
+        return head1
+    p_1 = head1
+    p_2 = head2
+    head = Node(0)
+    p = head
+    while p_1 and p_2:
+        if p_1.value < p_2.value:
+            p.next = p_1
+            p = p.next
+            p_1 = p_1.next
+        else:
+            p.next = p_2
+            p = p.next
+            p_2 = p_2.next
+    if p_1:
+        p.next = p_1
+    elif p_2:
+        p.next = p_2
+
+    return head.next
+
+
+def merge_two_sorted_list2(head1, head2):
+    if head1 is None:
+        return head2
+    if head2 is None:
+        return head1
+    p1 = head1
+    p2 = head2
+
+    if p1.value > p2.value:
+        new_head = p2
+        new_head.next = merge_two_sorted_list2(p1, p2.next)
+    else:
+        new_head = p1
+        new_head.next = merge_two_sorted_list2(p1.next, p2)
+
+    return new_head
+
+
+def reverse_link_list(head):
+    if head is None or head.next is None:
+        return head
+    p = head.next
+    p_last = head
+    p_next = p.next
+    if p_next is None:
+        head.next = None
+        p.next = head
+        return p
+    head.next = None
+    while p_next is not None:
+        p.next = p_last
+        p_last = p
+        p = p_next
+        p_next = p.next
+    p.next = p_last
+
+    return p
+
+
+def reverse_link_list2(head):
+    if head is None or head.next is None:
+        return head
+    new_head = Node(0)
+    p = head
+    while p is not None:
+        tmp = p.next
+        p.next = new_head.next
+        new_head.next = p
+        p = tmp
+
+    return new_head.next
+
+
+def reverse_link_list3(head):
+    if head is None or head.next is None:
+        return head
+    reverse_head_sub = reverse_link_list3(head.next)
+    head.next.next = head
+    head.next = None
+    return reverse_head_sub
+
+
+def test_merge_two_sorted_list():
+    head1 = gene_link_list([1, 2, 4, 5, 8, 9])
+    head2 = gene_link_list([2, 4, 7, 8, 12, 15])
+    print_link_list_value(head1)
+    print_link_list_value(head2)
+    head = merge_two_sorted_list2(head1, head2)
+    print_link_list_value(head)
+
+
+def test_reverse_link_list():
+    head = gene_link_list([1, 2, 4, 5, 8, 9])
+    head = reverse_link_list3(head)
+    print_link_list_value(head)
+
+
+def test_get_last_k():
+    head = gene_link_list([1, 2, 4, 5, 8, 9])
+    head_last_k = get_last_k(head, 3)
+    print_link_list_value(head_last_k)
+
+
+if __name__ == "__main__":
+    test_merge_two_sorted_list()
---
+++ sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(list):
+    return list
+
+
+def test_bubble_sort(list):
+    a = [1, 5, 3, 9, 21, 2, 8, 4, 7, 16, 6, 10]
+    print(bubble_sort(a))
+
+
+if __name__ == "__main__":
+    test_bubble_sort()
